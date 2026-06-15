8a56e4c5f8d1593a943f03fad13ae4a1956cb6c1
saker <sakertooth@gmail.com>
2026-06-14 21:28:53 -0400

    add dequeue tests, check return values from partial enqueue/dequeue functions

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -58,7 +58,7 @@ TEST_CASE("Enqueue operations")
         constexpr auto elements = std::array{1, 2, 3, 4, 5, 6};
         auto queue = spsc::LockfreeSpscQueue<int, 32>{};
 
-        CHECK(queue.enqueueSome(elements));
+        CHECK(queue.enqueueSome(elements) == elements.size());
         CHECK(queue.size() == elements.size());
         CHECK(queue.peek() == elements.front());
     }
@@ -69,8 +69,84 @@ TEST_CASE("Enqueue operations")
         constexpr auto elements = std::array{1, 2, 3, 4, 5, 6};
         auto queue = spsc::LockfreeSpscQueue<int, 3>{};
 
-        CHECK(queue.enqueueSome(elements));
+        CHECK(queue.enqueueSome(elements) == queue.capacity());
         CHECK(queue.size() == queue.capacity());
         CHECK(queue.peek() == elements.front());
+    }
+}
+
+TEST_CASE("Dequeue operations")
+{
+    SUBCASE("Dequeuing single element")
+    {
+        auto queue = spsc::LockfreeSpscQueue<int, 32>{};
+        auto value = 0;
+
+        queue.enqueue(1);
+
+        CHECK(queue.dequeue(value));
+        CHECK(value == 1);
+    }
+
+    SUBCASE("Dequeuing single element when queue is empty")
+    {
+        auto queue = spsc::LockfreeSpscQueue<int, 32>{};
+        auto value = 0;
+
+        CHECK(!queue.dequeue(value));
+        CHECK(value == 0);
+    }
+
+    SUBCASE("Dequeuing all batch of elements requested")
+    {
+        auto queue = spsc::LockfreeSpscQueue<int, 3>{};
+        auto values = std::array<int, 2>{};
+
+        queue.enqueueAll(std::array{1, 2});
+
+        CHECK(queue.dequeueAll(values));
+        CHECK(values[0] == 1);
+        CHECK(values[1] == 2);
+    }
+
+    SUBCASE("Dequeuing all batch of elements (not enough elements)")
+    {
+        auto queue = spsc::LockfreeSpscQueue<int, 4>{};
+        auto values = std::array<int, 4>{};
+
+        queue.enqueueAll(std::array{1, 2});
+
+        CHECK(!queue.dequeueAll(values));
+        CHECK(values[0] == 0);
+        CHECK(values[1] == 0);
+        CHECK(values[2] == 0);
+        CHECK(values[3] == 0);
+    }
+
+    SUBCASE("Dequeuing some batch of elements")
+    {
+        auto queue = spsc::LockfreeSpscQueue<int, 4>{};
+        auto values = std::array<int, 3>{};
+
+        queue.enqueueAll(std::array{1, 2, 3});
+
+        CHECK(queue.dequeueSome(values) == values.size());
+        CHECK(values[0] == 1);
+        CHECK(values[1] == 2);
+        CHECK(values[2] == 3);
+    }
+
+    SUBCASE("Dequeuing some batch of elements (not enough elements)")
+    {
+        auto queue = spsc::LockfreeSpscQueue<int, 4>{};
+        auto values = std::array<int, 4>{};
+
+        queue.enqueueAll(std::array{1, 2});
+
+        CHECK(queue.dequeueSome(values) == 2);
+        CHECK(values[0] == 1);
+        CHECK(values[1] == 2);
+        CHECK(values[2] == 0);
+        CHECK(values[3] == 0);
     }
 }
