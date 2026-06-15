3ab56a090e22107cd96b7f080ca6a81ea929b4f5
saker <sakertooth@gmail.com>
2026-06-15 14:11:23 -0400

    fix single throughput benchmark, use fixtures

--- benchmarks/benchmarks.cpp
+++ benchmarks/benchmarks.cpp
@@ -2,44 +2,95 @@
 #include <benchmark/benchmark.h>
 #include <emmintrin.h>
 
-template <std::size_t N>
-static void BM_Throughput_Single(benchmark::State &state) {
-  static auto queue = std::unique_ptr<spsc::LockfreeSpscQueue<int, N>>{};
-  static auto itemsProcessed = 0;
-  const auto itemToEnqueue = 42;
+template <std::size_t N> struct SpscFixture : public benchmark::Fixture {
+  inline static auto queue = std::shared_ptr<spsc::LockfreeSpscQueue<int, N>>{};
+  inline static auto itemsProcessed = std::size_t{0};
 
-  if (state.thread_index() == 0) {
-    queue = std::make_unique<spsc::LockfreeSpscQueue<int, N>>();
+  void SetUp(benchmark::State &state) override {
+    if (state.thread_index() == 0) {
+      queue = std::make_shared<spsc::LockfreeSpscQueue<int, N>>();
+    }
   }
 
+  void TearDown(benchmark::State &state) override {
+    if (state.thread_index() == 0) {
+      state.SetItemsProcessed(itemsProcessed);
+      queue.reset();
+    }
+  }
+};
+
+template <std::size_t N>
+static void BM_Throughput_Single(benchmark::State &state,
+                                 SpscFixture<N> *fixture) {
   for (auto _ : state) {
     if (state.thread_index() == 0) {
-      while (!queue->enqueue(itemToEnqueue)) {
+      constexpr auto itemToEnqueue = 42;
+      while (!fixture->queue->enqueue(itemToEnqueue)) {
         _mm_pause();
       }
-    }
-
-    if (state.thread_index() == 1) {
-      while (!queue->dequeue()) {
+    } else if (state.thread_index() == 1) {
+      while (!fixture->queue->dequeue()) {
         _mm_pause();
       }
 
-      ++itemsProcessed;
+      ++fixture->itemsProcessed;
     }
-  }
-
-  if (state.thread_index() == 0) {
-    queue.reset();
-    state.SetItemsProcessed(itemsProcessed);
   }
 }
 
-BENCHMARK(BM_Throughput_Single<128>)->Threads(2)->UseRealTime();
-BENCHMARK(BM_Throughput_Single<512>)->Threads(2)->UseRealTime();
-BENCHMARK(BM_Throughput_Single<1024>)->Threads(2)->UseRealTime();
+BENCHMARK_TEMPLATE_DEFINE_F(SpscFixture, BM_Throughput_Single_100,
+                            100)(benchmark::State &state) {
+  BM_Throughput_Single<100>(state, this);
+}
 
-BENCHMARK(BM_Throughput_Single<100>)->Threads(2)->UseRealTime();
-BENCHMARK(BM_Throughput_Single<500>)->Threads(2)->UseRealTime();
-BENCHMARK(BM_Throughput_Single<1000>)->Threads(2)->UseRealTime();
+BENCHMARK_TEMPLATE_DEFINE_F(SpscFixture, BM_Throughput_Single_128,
+                            128)(benchmark::State &state) {
+  BM_Throughput_Single<128>(state, this);
+}
+
+BENCHMARK_TEMPLATE_DEFINE_F(SpscFixture, BM_Throughput_Single_500,
+                            500)(benchmark::State &state) {
+  BM_Throughput_Single<500>(state, this);
+}
+
+BENCHMARK_TEMPLATE_DEFINE_F(SpscFixture, BM_Throughput_Single_512,
+                            512)(benchmark::State &state) {
+  BM_Throughput_Single<512>(state, this);
+}
+
+BENCHMARK_TEMPLATE_DEFINE_F(SpscFixture, BM_Throughput_Single_1000,
+                            1000)(benchmark::State &state) {
+  BM_Throughput_Single<1000>(state, this);
+}
+
+BENCHMARK_TEMPLATE_DEFINE_F(SpscFixture, BM_Throughput_Single_1024,
+                            1024)(benchmark::State &state) {
+  BM_Throughput_Single<1024>(state, this);
+}
+
+BENCHMARK_REGISTER_F(SpscFixture, BM_Throughput_Single_100)
+    ->Threads(2)
+    ->UseRealTime();
+
+BENCHMARK_REGISTER_F(SpscFixture, BM_Throughput_Single_128)
+    ->Threads(2)
+    ->UseRealTime();
+
+BENCHMARK_REGISTER_F(SpscFixture, BM_Throughput_Single_500)
+    ->Threads(2)
+    ->UseRealTime();
+
+BENCHMARK_REGISTER_F(SpscFixture, BM_Throughput_Single_512)
+    ->Threads(2)
+    ->UseRealTime();
+
+BENCHMARK_REGISTER_F(SpscFixture, BM_Throughput_Single_1000)
+    ->Threads(2)
+    ->UseRealTime();
+
+BENCHMARK_REGISTER_F(SpscFixture, BM_Throughput_Single_1024)
+    ->Threads(2)
+    ->UseRealTime();
 
 BENCHMARK_MAIN();
