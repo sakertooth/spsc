ad634374f8fa4c335f8a98b279b47f6ed7ac2d07
saker <sakertooth@gmail.com>
2026-06-15 05:42:11 -0400

    add benchmarks for throughput on single enqueue/dequeue

--- benchmarks/benchmarks.cpp
+++ benchmarks/benchmarks.cpp
@@ -1,3 +1,45 @@
-#include <benchmark/benchmark.h>
 #include "spsc.hpp"
-#include <thread>
+#include <benchmark/benchmark.h>
+#include <emmintrin.h>
+
+template <std::size_t N>
+static void BM_Throughput_Single(benchmark::State &state) {
+  static auto queue = std::unique_ptr<spsc::LockfreeSpscQueue<int, N>>{};
+  static auto itemsProcessed = 0;
+  const auto itemToEnqueue = 42;
+
+  if (state.thread_index() == 0) {
+    queue = std::make_unique<spsc::LockfreeSpscQueue<int, N>>();
+  }
+
+  for (auto _ : state) {
+    if (state.thread_index() == 0) {
+      while (!queue->enqueue(itemToEnqueue)) {
+        _mm_pause();
+      }
+    }
+
+    if (state.thread_index() == 1) {
+      while (!queue->dequeue()) {
+        _mm_pause();
+      }
+
+      ++itemsProcessed;
+    }
+  }
+
+  if (state.thread_index() == 0) {
+    queue.reset();
+    state.SetItemsProcessed(itemsProcessed);
+  }
+}
+
+BENCHMARK(BM_Throughput_Single<128>)->Threads(2)->UseRealTime();
+BENCHMARK(BM_Throughput_Single<512>)->Threads(2)->UseRealTime();
+BENCHMARK(BM_Throughput_Single<1024>)->Threads(2)->UseRealTime();
+
+BENCHMARK(BM_Throughput_Single<100>)->Threads(2)->UseRealTime();
+BENCHMARK(BM_Throughput_Single<500>)->Threads(2)->UseRealTime();
+BENCHMARK(BM_Throughput_Single<1000>)->Threads(2)->UseRealTime();
+
+BENCHMARK_MAIN();
